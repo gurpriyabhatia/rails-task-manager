392367bfe8f41697203181aa68b98906f8c05064
Gurpriya Bhatia <gurpriya.bhatia@gmail.com>
2020-01-18 16:21:11 +0000

    Rails Task Manager completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :tasks
+  # get 'tasks', to: 'tasks#index' # as: :tasks
+  # get 'tasks/new', to: 'tasks#new'
+
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :taskedit
+  # patch 'tasks/:id', to: 'tasks#update'
+
+  # post 'tasks', to: 'tasks#create'
+
+  # delete 'tasks/:id', to: 'tasks#destroy', as: :taskdelete
+
+  # get 'tasks/:id', to: 'tasks#show', as: :task
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+task_2 = Task.new(title: "Buy all groceries", details: "Decide what I want to cook, create ingredient list and buy")
+task_2.save
+
+
+task_4 = Task.create(title: "Finish lecture", details: "Watch the lecture before the class")
